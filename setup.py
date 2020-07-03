a5f301f526591373641e5c27cf32d82b60190817
Gabriele Turelli <cappy95@gmail.com>
2020-07-03 15:51:30 +0200

    PEP Style formatting

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-import cx_Freeze
 from cx_Freeze import *
 
 
 setup(
-	name = "G14Control",
-    version = "0.1",
-	options = {'buid_exe':{'packages':['resources','pystray._win32']}},
-	executables=[
-		Executable(
-			script="G14Control.pyw",
+    name="G14Control",
+    version="0.1",
+    options={'buid_exe': {'packages': ['resources', 'pystray._win32']}},
+    executables=[
+        Executable(
+            script="G14Control.pyw",
             icon="res\icon.ico",
-            base = "Win32GUI"
-			)
-		]
-	)
+            base="Win32GUI"
+            )
+        ]
+    )
